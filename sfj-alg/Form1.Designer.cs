843a3b18e090cce532cff45240b46a0c232e5018
mahy209 <60543525+mahy209@users.noreply.github.com>
2021-05-10 14:56:55 +0200

    myfile

---
+++ sfj-alg/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace sfj_alg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "enter number \r\nof process\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "enter burst time\r\nof the process\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(253, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "enter process\r\narrival time\r\n";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(132, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "average waiting\r\n time";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(297, 94);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 28);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(297, 147);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 28);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(406, 231);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 28);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(76, 231);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 23);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "preemptive";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(275, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "insert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(297, 361);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(169, 38);
+            this.textBox1.TabIndex = 9;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(134, 432);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(418, 54);
+            this.flowLayoutPanel1.TabIndex = 10;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(134, 492);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(418, 53);
+            this.flowLayoutPanel2.TabIndex = 11;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(718, 689);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ sfj-alg/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sfj_alg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int num_process;
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            num_process = (int)numericUpDown1.Value;
+
+            // reset and start again
+            button1.Enabled = true;
+            // gant.Clear();
+            flowLayoutPanel2.Controls.Clear();
+            flowLayoutPanel1.Controls.Clear();
+            sjwtime = 0;
+            sjaverageWtime = 0;
+            prsjaverageWtime = 0;
+            prsjwtime = 0;
+            pd = 1;
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label3.Visible = true;
+                numericUpDown3.Visible = true;
+            }
+            else
+            {
+                label3.Visible = false;
+                numericUpDown3.Visible = false;
+            }
+        }
+
+        // store values accending
+        double sjwtime = 0, sjaverageWtime = 0, prsjaverageWtime = 0, prsjwtime = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process x = new Process(); // dummy process
+            x.Pid = pd;
+            pd++;
+            x.burst_time = (int)numericUpDown2.Value;
+            if (checkBox1.Checked) x.arrival_time = (int)numericUpDown3.Value;
+            vals.Add(x);
+            string ss_sjf = "Inserted " + (vals.Count.ToString()) + " from " + (num_process.ToString());
+            MessageBox.Show(ss_sjf);
+
+            if (vals.Count == num_process && !checkBox1.Checked)
+            {
+                button1.Enabled = false;
+                vals = vals.OrderBy(arr => arr.burst_time).ToList();
+                int st_t = 0;
+                for (int j = 0; j < num_process; j++)
+                {
+                    //gantt chart code
+                    ///////// type time lable
+                    Label num_fcfs = new Label();
+                    num_fcfs.Location = new Point(10 + st_t * 10, 305);
+                    num_fcfs.Text = st_t.ToString();
+                    if (vals[j].burst_time < 3) num_fcfs.Width = 28;
+                    else num_fcfs.Width = vals[j].burst_time * 10 - 2;
+                    flowLayoutPanel2.Controls.Add(num_fcfs);
+                    /////////
+                    st_t += vals[j].burst_time;
+                    //// type process number lable
+                    Label tempLabel_fcfs = new Label();
+                    tempLabel_fcfs.Enabled = true;
+                    tempLabel_fcfs.BorderStyle = BorderStyle.FixedSingle;
+                    tempLabel_fcfs.Font = new Font("Arial", 10, FontStyle.Bold);
+                    tempLabel_fcfs.Location = new Point(10 + time_fcfs * 10, 300);
+                    string s = "P" + vals[j].Pid.ToString();
+                    tempLabel_fcfs.Text = s;
+                    tempLabel_fcfs.TextAlign = ContentAlignment.MiddleCenter;
+                    if (vals[j].burst_time < 3) tempLabel_fcfs.Width = 30;
+                    else tempLabel_fcfs.Width = vals[j].burst_time * 10;
+                    flowLayoutPanel1.Controls.Add(tempLabel_fcfs);
+                    ////////////////////////
+
+                    //cal average waiting time
+                    sjaverageWtime += sjwtime;
+                    sjwtime += vals[j].burst_time;
+                }
+                //print last timelable in gant
+                Label num_fcfss = new Label();
+                num_fcfss.Location = new Point(10 + st_t * 10, 300);
+                num_fcfss.Text = st_t.ToString();
+                flowLayoutPanel2.Controls.Add(num_fcfss);
+                /////////////////
+
+                //print average time
+                textBox1.Text = (sjaverageWtime / (double)num_process).ToString() + " msec";
+                vals.Clear();
+            }
+
+            //preemtive code
+            else if (vals.Count == num_process && checkBox1.Checked)
+            {
+                button1.Enabled = false;
+                vals = vals.OrderBy(arr => arr.arrival_time).ToList();
+                int indx = 0;
+                for (int i = 0; i < vals.Count - 1; i++)
+                {
+                    Process pre_sjf = new Process();
+                    pre_sjf.burst_time = vals[i + 1].arrival_time - vals[i].arrival_time;
+                    pre_sjf.Pid = vals[indx].Pid;
+                    pre_sjf.arrival_time = vals[indx].arrival_time;
+                    premtive_vals.Add(pre_sjf);
+                    vals[indx].burst_time -= pre_sjf.burst_time;
+                    int bst_tm = vals[indx].burst_time;
+                    for (int j = i + 1; j >= 0; j--)
+                        if (vals[j].burst_time < bst_tm) indx = j;
+                }
+
+                vals = vals.OrderBy(arr => arr.burst_time).ToList();
+                for (int xx = 0; xx < vals.Count; xx++) premtive_vals.Add(vals.ElementAt(xx));//join lists
+                for (int jj = 0; jj < premtive_vals.Count - 1; jj++)//join similar PID
+                {
+                    if (premtive_vals[jj].Pid == premtive_vals[jj + 1].Pid)
+                    {
+                        premtive_vals[jj + 1].burst_time += premtive_vals[jj].burst_time;
+                        premtive_vals.RemoveAt(jj);
+                        jj = -1;
+                    }
+                }
+
+                int st_t = 0; int bs = 0;
+                for (int j = 0; j < premtive_vals.Count; j++)
+                {
+                    //gantt chart code
+                    ///////// type time lable
+                    Label num_fcfs = new Label();
+                    num_fcfs.Location = new Point(10 + st_t * 10, 305);
+                    num_fcfs.Text = st_t.ToString();
+                    if (premtive_vals[j].burst_time < 3) num_fcfs.Width = 28;
+                    else num_fcfs.Width = premtive_vals[j].burst_time * 10 - 2;
+                    flowLayoutPanel2.Controls.Add(num_fcfs);
+                    /////////
+                    st_t += premtive_vals[j].burst_time;
+                    //// type process number lable
+                    Label tempLabel_fcfs = new Label();
+                    tempLabel_fcfs.Enabled = true;
+                    tempLabel_fcfs.BorderStyle = BorderStyle.FixedSingle;
+                    tempLabel_fcfs.Font = new Font("Arial", 10, FontStyle.Bold);
+                    tempLabel_fcfs.Location = new Point(10 + time_fcfs * 10, 300);
+                    string s = "P" + premtive_vals[j].Pid.ToString();
+                    tempLabel_fcfs.Text = s;
+                    tempLabel_fcfs.TextAlign = ContentAlignment.MiddleCenter;
+                    if (premtive_vals[j].burst_time < 3) tempLabel_fcfs.Width = 30;
+                    else tempLabel_fcfs.Width = premtive_vals[j].burst_time * 10;
+                    flowLayoutPanel1.Controls.Add(tempLabel_fcfs);
+                    ////////////////////////
+
+                    //cal average waiting time
+                    for (int z = j - 1; z >= 0; z--)
+                        if (premtive_vals[j].Pid == premtive_vals[z].Pid)
+                            bs = premtive_vals[z].burst_time;
+                    prsjaverageWtime += (prsjwtime - bs - premtive_vals[j].arrival_time);
+                    bs = 0;
+                    prsjwtime += premtive_vals[j].burst_time;
+                }
+
+                //print last timelable in gant
+                Label num_fcfss = new Label();
+                num_fcfss.Location = new Point(10 + st_t * 10, 300);
+                num_fcfss.Text = st_t.ToString();
+                flowLayoutPanel2.Controls.Add(num_fcfss);
+                /////////////////
+
+                //print avg
+                textBox1.Text = (prsjaverageWtime / (double)num_process).ToString() + " msec";
+                vals.Clear();
+                premtive_vals.Clear();
+            }
+
+        }
+        class Process
+        {
+            public int Pid;
+            public int burst_time;
+            public int waiting_time = 0;
+            public int last_active = 0;
+            public int arrival_time;
+            public int priority;
+
+        }
+
+        List<Process> vals = new List<Process>();
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        List<Process> premtive_vals = new List<Process>();
+        int pd = 1;
+
+        public int time_fcfs { get; private set; }
+    }
+}
+
